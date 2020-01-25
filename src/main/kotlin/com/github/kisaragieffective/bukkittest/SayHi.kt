782198b100308cbc92598b0cba2d1c146a3a1ec9
KisaragiEffective <kisaragi.effective@gmail.com>
2020-01-25 23:07:05 +0900

    Initial commit

---
+++ src/main/kotlin/com/github/kisaragieffective/bukkittest/SayHi.kt
@@ -0,0 +1,4 @@
+package com.github.kisaragieffective.bukkittest
+
+fun main() {
+}
---
+++ src/test/kotlin/com/github/kisaragieffective/bukkittest/WorldTest.kt
@@ -0,0 +1,15 @@
+package com.github.kisaragieffective.bukkittest
+
+import org.bukkit.craftbukkit.v1_12_R1.CraftServer
+import org.bukkit.craftbukkit.v1_12_R1.CraftWorld
+import org.jetbrains.annotations.TestOnly
+import org.junit.jupiter.api.Test
+
+class WorldTest {
+    private val craft: Nothing = TODO()
+    @TestOnly
+    @Test
+    fun check() {
+
+    }
+}
